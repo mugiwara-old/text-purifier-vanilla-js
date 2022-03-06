f749a358599ec46a62ee63fb1b30036344479710
mugiwarafx <sangalesbenes@uoc.edu>
2022-03-06 19:21:17 +0100

    adds updateClipboard functionality

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-function updateValue(e) {
-    console.log(e.target.value.length)
-    const btn = document.getElementById('button')
+function updateNotification(e) {
+    const notification = document.getElementById('notification')
     let string = '';
     if (e.target.value.length === 0) string = 'Just paste your grubby text!'
-    else string = 'Your formatted text has been converted to plain text!'
-    btn.innerHTML = string
+    else string = 'Your formatted text has been converted to plain text and pasted to your navigator clipboard!'
+    notification.innerHTML = string
 }
+
+async function updateClipboard(e) {
+  const navigtorClipboard = await navigator.clipboard.readText()
+  if(navigtorClipboard.length === 0) return;
+  const copyText = document.getElementById('textarea')
+  copyText.select()
+  copyText.setSelectionRange(0, 99999) /* For mobile devices */
+  await navigator.clipboard.writeText(copyText.value)
+}
+
 const textarea = document.getElementById('textarea')
-textarea.onkeyup = updateValue
+textarea.onkeyup = updateNotification
+textarea.onpaste = updateClipboard
